3ac070d964c2f44e4ce823d1a894c5ff4e8b9153
King Igbozuruike <kingobiomaeze2020@gmail.com>
2024-03-11 20:06:05 +0000

    edited CSS to make site appealing.

--- code-files/app.js
+++ code-files/app.js
@@ -9,14 +9,33 @@ taskForm.addEventListener("submit", function (event) {
     
     if(taskForm !==""){
         const taskItem = document.createElement("li");
-        taskItem.classList.add("task-item");
+        taskItem.classList.add("task-item"); 
         taskItem.textContent = taskText;
         
        taskItem.addEventListener("click", function () {
         this.classList.toggle("completed")
        });
+    
+       
+       taskItem.addEventListener("dblclick", function () {
+        const taskText = this.textContent;
+        const inputField = document.createElement("input");
+        inputField.type = "text"
+        inputField.value = taskText;
+        
+        this.textContent = "";
+        this.appendChild(inputField);
+
+        inputField.addEventListener("blur", function () {
+            const newTasktext = this.value;
+            taskItem.textContent = newTasktext
+        })
+       })
         
         taskList.appendChild(taskItem);
         taskInput.value = "";
     }
 });
+
+
+
